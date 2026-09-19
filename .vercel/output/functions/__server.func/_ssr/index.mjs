0352db09d5d979dfb37c986d63a199e31aef8f09
Asif556 <amd90982@gmail.com>
2026-09-19 12:07:22 +0530

    chore: add Vercel build output and serverless functions

--- .vercel/output/functions/__server.func/_ssr/index.mjs
+++ .vercel/output/functions/__server.func/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-DyYIupMM.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-GovPWat-.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
